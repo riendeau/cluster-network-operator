fb5234524b00ae97de9cc46ae6e6df7d90c42fb7
John Riendeau <riendeau@us.ibm.com>
2024-06-12 19:13:46 -0500

    Add unit test

--- pkg/network/multus_admission_controller_test.go
+++ pkg/network/multus_admission_controller_test.go
@@ -1,16 +1,21 @@
 package network
 
 import (
-	"github.com/openshift/cluster-network-operator/pkg/hypershift"
 	"testing"
+
+	"github.com/openshift/cluster-network-operator/pkg/hypershift"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	. "github.com/onsi/gomega"
 	operv1 "github.com/openshift/api/operator/v1"
 	"github.com/openshift/cluster-network-operator/pkg/names"
 
 	cnofake "github.com/openshift/cluster-network-operator/pkg/client/fake"
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	uns "k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 var MultusAdmissionControllerConfig = operv1.Network{
@@ -85,8 +90,18 @@ func TestRenderMultusAdmissionController(t *testing.T) {
 	g.Expect(objs).To(ContainElement(HaveKubernetesID("Deployment", "openshift-multus", "multus-admission-controller")))
 }
 
-// TestRenderMultusAdmissionController has some simple rendering tests
+// TestRenderMultusAdmissonControllerConfigForHyperShift has some simple rendering tests
 func TestRenderMultusAdmissonControllerConfigForHyperShift(t *testing.T) {
+	doTestRenderMultusAdmissonControllerConfigForHyperShift(t, false)
+}
+
+// TestRenderMultusAdmissonControllerConfigForHyperShiftWithRequests tests that changes to
+// multus container resource requests are preserved
+func TestRenderMultusAdmissonControllerConfigForHyperShiftWithAlteredResourceRequests(t *testing.T) {
+	doTestRenderMultusAdmissonControllerConfigForHyperShift(t, true)
+}
+
+func doTestRenderMultusAdmissonControllerConfigForHyperShift(t *testing.T, addMultusDeployment bool) {
 	g := NewGomegaWithT(t)
 
 	crd := MultusAdmissionControllerConfig.DeepCopy()
@@ -95,7 +110,7 @@ func TestRenderMultusAdmissonControllerConfigForHyperShift(t *testing.T) {
 	config.DisableMultiNetwork = &disabled
 	fillDefaults(config, nil)
 
-	fakeClient := cnofake.NewFakeClient(
+	clientObjs := []client.Object{
 		&corev1.Namespace{
 			ObjectMeta: metav1.ObjectMeta{
 				Name: "test1-ignored",
@@ -127,14 +142,15 @@ func TestRenderMultusAdmissonControllerConfigForHyperShift(t *testing.T) {
 				"MyCMKey": "key",
 			},
 		},
-		&corev1.Namespace{ObjectMeta: metav1.ObjectMeta{
-			Name: "test3-ignored",
-			Labels: map[string]string{
-				"openshift.io/cluster-monitoring": "true",
+		&corev1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test3-ignored",
+				Labels: map[string]string{
+					"openshift.io/cluster-monitoring": "true",
+				},
 			},
 		},
-		})
-	bootstrap := fakeBootstrapResultWithHyperShift()
+	}
 
 	hsc := hypershift.NewHyperShiftConfig()
 	hsc.Enabled = true
@@ -146,12 +162,44 @@ func TestRenderMultusAdmissonControllerConfigForHyperShift(t *testing.T) {
 	hsc.ReleaseImage = "MyImage"
 	hsc.ControlPlaneImage = "MyCPOImage"
 
+	expectedCpuReq := "10m"
+	expectedMemReq := "50Mi"
+	if addMultusDeployment {
+		expectedCpuReq = "20m"
+		expectedMemReq = "75Mi"
+		clientObjs = append(clientObjs, &appsv1.Deployment{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "multus-admission-controller",
+				Namespace: hsc.Namespace,
+			},
+			Spec: appsv1.DeploymentSpec{
+				Template: corev1.PodTemplateSpec{
+					Spec: corev1.PodSpec{
+						Containers: []corev1.Container{{
+							Name: "multus-admission-controller",
+							Resources: corev1.ResourceRequirements{
+								Requests: corev1.ResourceList{
+									corev1.ResourceCPU:    resource.MustParse(expectedCpuReq),
+									corev1.ResourceMemory: resource.MustParse(expectedMemReq),
+								},
+							},
+						}},
+					},
+				},
+			},
+		})
+	}
+
+	fakeClient := cnofake.NewFakeClient(clientObjs...)
+	bootstrap := fakeBootstrapResultWithHyperShift()
+
 	objs, err := renderMultusAdmissonControllerConfig(manifestDir, false, bootstrap, fakeClient, hsc, "")
 	g.Expect(err).NotTo(HaveOccurred())
 
 	// Check rendered object
 	for _, obj := range objs {
-		if obj.GetKind() == "Service" && obj.GetName() == "multus-admission-controller" {
+		switch {
+		case obj.GetKind() == "Service" && obj.GetName() == "multus-admission-controller":
 			labels := obj.GetLabels()
 			g.Expect(len(labels)).To(Equal(2))
 			g.Expect(labels["hypershift.openshift.io/allow-guest-webhooks"]).To(Equal("true"))
@@ -159,6 +207,33 @@ func TestRenderMultusAdmissonControllerConfigForHyperShift(t *testing.T) {
 			annotations := obj.GetAnnotations()
 			g.Expect(len(annotations)).To(Equal(1))
 			g.Expect(annotations["network.operator.openshift.io/cluster-name"]).To(Equal("management"))
+
+		case obj.GetKind() == "Deployment" && obj.GetName() == "multus-admission-controller":
+			// Check the resource request values
+			containers, found, err := uns.NestedSlice(obj.Object, "spec", "template", "spec", "containers")
+			g.Expect(err).NotTo(HaveOccurred())
+			g.Expect(found).To(BeTrue())
+
+			var mac map[string]interface{}
+			for _, container := range containers {
+				cmap := container.(map[string]interface{})
+				name, found, err := uns.NestedString(cmap, "name")
+				if found && err == nil && name == "multus-admission-controller" {
+					mac = cmap
+					break
+				}
+			}
+			g.Expect(mac).ToNot(BeNil())
+
+			cpuReq, found, err := uns.NestedString(mac, "resources", "requests", "cpu")
+			g.Expect(err).NotTo(HaveOccurred())
+			g.Expect(found).To(BeTrue())
+			g.Expect(cpuReq).To(Equal(expectedCpuReq))
+
+			memReq, found, err := uns.NestedString(mac, "resources", "requests", "memory")
+			g.Expect(err).NotTo(HaveOccurred())
+			g.Expect(found).To(BeTrue())
+			g.Expect(memReq).To(Equal(expectedMemReq))
 		}
 	}
 }
